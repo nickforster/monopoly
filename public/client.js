3a69635cbd92a80c66b6e2e262caffcf5b804509
nickforster <nick_forster@icloud.com>
2022-05-23 13:40:15 +0200

    login done

--- public/client.js
+++ public/client.js
@@ -1,1 +1,38 @@
 const socket = io()
+
+// Different screens of the website
+const start = document.getElementById('start')
+const lobby = document.getElementById('lobby')
+const game = document.getElementById('game')
+
+/**
+ * Start screen
+ *
+ * Validation of player and room name
+ * Joining a Room
+ */
+const joinRoom = document.getElementById('join-room')
+const playerName = document.getElementById('player-name')
+const roomName = document.getElementById('room-name')
+const errorEmptyInputs = document.getElementById('error-empty-inputs')
+const errorFullRoom = document.getElementById('error-full-room')
+
+joinRoom.addEventListener('click', () => {
+    console.log("clicked")
+    if (playerName.value !== '' && roomName.value !== '') {
+        socket.emit('join-room', playerName.value, socket.id, roomName.value)
+        start.classList.add('hidden')
+        lobby.classList.remove('hidden')
+        game.classList.remove('hidden')
+    } else {
+        errorEmptyInputs.classList.remove('hidden');
+    }
+})
+
+socket.on('room-full', () => {
+    start.classList.remove('hidden')
+    lobby.classList.add('hidden')
+    game.classList.add('hidden')
+    errorEmptyInputs.classList.add('hidden');
+    errorFullRoom.classList.remove('hidden')
+})
--- server.js
+++ server.js
@@ -10,15 +10,54 @@ const io = socketIo(server)
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Object which holds all rooms with the socket ids and player names
+let rooms = {}
+//hold all players with their rooms
+let players = {}
+//amount of players allowed per room
+const MAX_PLAYERS = 4
+
 // run when client connects
 io.on('connection', (socket) => {
     console.log("New socket joined")
 
+    /**
+     * Join a room
+     */
+    socket.on('join-room', (name, id, room) => {
+        if (rooms[room] === undefined) {
+            rooms[room] = {}
+            rooms[room][id] = name
+            players[id] = room
+            socket.join(room)
+        } else if (Object.keys(rooms[room]).length < MAX_PLAYERS) {
+            rooms[room][id] = name
+            players[id] = room
+            socket.join(room)
+        } else {
+            io.in(id).emit('room-full')
+        }
+    })
+
+
+    /**
+     * Socket disconnects
+     */
     socket.on('disconnect', () => {
-        console.log('Socket disconnected')
+        leaveRoom(socket)
     })
 })
 
-const PORT = process.env.PORT || 3000;
+function leaveRoom(socket) {
+    const room = players[socket.id]
+    delete players[socket.id]
+    delete rooms[room][socket.id]
+    socket.leave(room)
+    console.log(players)
+    console.log(rooms)
+}
 
+
+// Port and listening of the server
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
